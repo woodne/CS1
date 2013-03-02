84578f97bd710e0a1565b2077ffe468ef3685566
Aesti <joshh@vandals.uidaho.edu>
2013-03-01 19:34:18 -0800

    words

--- lab5.cpp
+++ lab5.cpp
@@ -2,31 +2,35 @@
 
 using namespace std;
 
-//function to take value and force user to repeat it if input invalid (choices up to me)
+//function to take value and force user to repeat it if input invalid
 int input(){
 	int choice = 0;
 	bool valid;
-	do{
-        cout << "Enter a number between 1-4. 1 = Red, 2 = Blue, , 3 = Orange, 4 = green." << endl;
+    do{
         cin >> choice;
         if(choice < 1 || choice > 4){
-    	    valid = false
+    	    valid = false;
     	    cout << "Invalid Input" << endl;
         }else{
     	    valid = true;
         }
     }while(!valid);
 
-
 	return choice;
 }
 
-//function to take value entered by user and return a fortune based on input
+//function to take values entered by user and return a fortune based on input
 int fortune(){
+
+    //perhaps add values together and have a series of if-statements to determine
+      //the fortune
 
 }
 
 int main(){
-	
+	cout <<"Enter a number between 1-4. What is your fav" <<
+        "orite supply to construct a spawning pool?" << endl;
+    cout << "-Excluding your initial 5 workers" << endl;
 	int choice = input();
+    cout <<"Enter a number between 1-4. "
 }
