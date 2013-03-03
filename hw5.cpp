2d2a1ec75fa787ddaf62ef2350011c9d1d99e034
Aesti <joshh@vandals.uidaho.edu>
2013-03-03 09:41:34 -0800

    Completed 1-6

--- hw5.cpp
+++ hw5.cpp
@@ -46,12 +46,44 @@ void pet::play(){
       hunger += 1;
       cout << "\nRARGGLGLLRBARKRKKCLUCKCLUCK\n";
       break;
+    case(4):
+      happy += -25;
+      hunger += 30;
+      cout << "\nBARKBARKWOOFMEOWSCRATCHCLAW! Your pet attacked another creature!\n";
+      break;
     default:
       cout << "Not a valid choice." << endl;
   }
 }
 //member function for feeding
 void pet::feed(){
+  int choice = 0;
+  cout << "What should we eat?\n";
+  cout << " Biscuits (1) \n Rat (2) \n Raccoon (3) \n Chipotle (4) \n";
+  switch(choice){
+    case(1):
+      happy += 5;
+      hunger += -5;
+      cout << "\nYour creature eats the biscuit.\n";
+      break;
+    case(2):
+      happy += 5;
+      hunger += -10;
+      cout << "\nYour creature ate a rat! Gross!!\n";
+      break;
+    case (3):
+      happy += 10;
+      hunger += -20;
+      cout << "\nYour creature ate a Raccoon! Poor furry creature. :(\n";
+      break;
+    case (4):
+      happy += 50;
+      hunger += -25;
+      cout << "\nYour creature ate Chipotle! You both will regret it later.\n";
+      break;
+    default:
+      cout << "Not a valid choice. " << endl;
+  }
   cout << "\nMMMM, Yummy!\n";
   hunger -=5;
 }
@@ -67,7 +99,7 @@ int pet::check_health(){
     return 1;
   }
   if(happy <= 0){
-    cout << "\nYour pet has died of a broken hearth.\n";
+    cout << "\nYour pet has died of a broken heart.\n";
     return 1;
   }
   return 0;
