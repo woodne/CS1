9a78808054116093ef0bd451f4a6dc124eb0a562
Aesti <joshh@vandals.uidaho.edu>
2013-03-04 11:14:19 -0800

    adding initial lab 7 commit

---
+++ lab7/lab7.cpp
@@ -0,0 +1,17 @@
+/* Josh Hartshorn
+   Lab 7 March 1st 2013
+   CS120
+
+   This program works with text files looking for certain text phrases among various other things.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+
+  return 0;
+}
