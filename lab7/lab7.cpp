3bc92d274a714e24347726e7c3f9f4b29f2bedee
Aesti <joshh@vandals.uidaho.edu>
2013-04-03 16:40:03 -0700

    finished assignment

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -3,33 +3,37 @@
    CS120
 
    This program works with text files looking for certain text phrases among various other things.
+    -Finished all requirements
 */
 
 #include <string>
 #include <iostream>
 #include <fstream>
-#include <algorithm> //std::search
 
 using namespace std;
 
 int main(){
   string line;
   ifstream dataFile;
-  int line_number = 0;
+  int line_number = 1;
+  int substring_count = 0;
   string word = "int ";
   dataFile.open("datafile.txt");
   if(dataFile.is_open()){
     while(dataFile.good()){
       getline (dataFile,line);
       cout << "\nLine# " << line_number << " :: ";
-      cout << "Line Length: " << line.length() << " :: ";
+      cout << "Line Length: " << line.length() << endl;;
       unsigned found = line.find(word); // if no matches it returns npos (-1)
       if(found!=string::npos){ //checks for npos, if none it found a match
-        cout << "\"" << word << "\"" <<" found on line: " << line_number << endl;
+        cout << "\t\"" << word << "\"" <<" found on line: " << line_number << endl;
+        cout << "\t\tCharacter After Substring: " << line[found + 4] << endl;
+        substring_count++;
       }
       line_number++; //increment line number
     }
     dataFile.close();
+    cout << "\n Total lines with substring: " << substring_count << endl;
   }
   return 0;
 }
