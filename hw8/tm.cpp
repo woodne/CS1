810ab73a06bdf5574f3f8ab662d6e2e3531192e5
Aesti <joshh@vandals.uidaho.edu>
2013-04-13 13:54:43 -0700

    just cleaning up output

--- hw8/tm.cpp
+++ hw8/tm.cpp
@@ -2,7 +2,7 @@
    CS120 Homework 8
    Terrain Manager Project
 
-   TODO: Make a menu and clean up output. Also, check diagonals in passable()
+   Make a menu and clean up output. Also, check diagonals in passable()
 */
 
 //save and load files
@@ -14,25 +14,44 @@ using namespace std;
 int main() {
   //create plot object
   Plot p;
-  string s = "turnip.dat";
+  string s;
+  cout << "Enter the name of datafile you want to load: ";
+  cin >> s;
   p.load(s);
-  cout << "\nTerrain:\n";
-  p.print_terrain_map();
-  cout << "\nElevation:\n";
-  p.print_elevation_map();
-  cout << "\n Minimum! \n";
-  p.minimum();
-  cout << "\n Maximum! \n";
-  p.maximum();
-  cout << endl;
-  int f = p.passable();
-  cout << endl << f << endl;
+  int choice;
+
+  bool quit = false;
+  while(!quit){
+    cout << "This is a primitive Terrain Manager." << endl;
+    cout << "Menu:" << endl;
+    cout << "\tEnter (1) to view the Height Map" << endl;
+    cout << "\tEnter (2) to view the Terrain Map" << endl; 
+    cout << "\tEnter (3) for maximum plot height" << endl;
+    cout << "\tEnter (4) for minimum plot height" << endl;
+    cout << "\tEnter (5) to determine if terrain is passable" << endl;
+    cout << "\tEnter (10) to quit" << endl;
+    cin >> choice;
+    switch(choice){
+      case 1: cout << "\nHeight Map:\n"; p.print_elevation_map(); 
+              break;
+      case 2: cout << "\nTerrain Map:\n"; p.print_terrain_map(); 
+              break;
+      case 3: cout << "\nMaximum:\n \t"; p.maximum(); 
+              break;
+      case 4: cout << "\nMinimum:\n \t"; p.minimum(); 
+              break;
+      case 5: int f = p.passable();
+              cout << "\nPassable:\n \t" << f << endl; 
+              break;
+      case 10: quit = true;
+      
+    }
+  }
   int a, b;
 
   int steepest = p.steepest(2,1);
   cout << "Steepest: " << steepest << endl;
   p.save("test");
-
 
   //cout << "Do you want to view a squares attributes? Enter the coordinates!\n";
   //cin >> a >> b;
