effe37769f43a37bad6fab0506020b0915c8ae02
Joshua Hartshorn <woodne@Nicholass-MacBook-Pro-2.local>
2013-05-24 19:59:30 -0700

    lab 13 need to do more research

--- lab13/lab13.cpp
+++ lab13/lab13.cpp
@@ -6,19 +6,39 @@
 using namespace std;
 class LLS{
 private:
-string str;
-LLS* next;
+  string str;
+  LLS* next;
 public:
   LLS(string s);
   LLS(string s, LLS *rest);
+  print();
+  append(string s);
 
 };
 LLS::LLS(string s){
-
-
+  str  = s;
+  next = NULL;
+}
+LLS::append(string s){
+if (next == NULL){
+  next = new LLS(s);
+}
+else{
+  next->append(string s);
+}
+}
+LLS::print(){
+  cout << str << endl;
+  next -> print();
 }
 
-int main(){
-
+int main(int argc and char *argv[]){
+  LLS *head = new LLS(argv[0]);
+  if (argc > 1){
+    for (int i = 0; i > -1; i--){
+      // call head->append(); on argv[i] or something
+    }
+  }
+  head->print();
   return 0;
 }
