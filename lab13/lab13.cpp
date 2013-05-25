d768a4488b9eecb991f203515cb53e50e40304ef
Joshua Hartshorn <woodne@Nicholass-MacBook-Pro-2.local>
2013-05-24 17:59:59 -0700

    initial lab 13 commit

---
+++ lab13/lab13.cpp
@@ -0,0 +1,24 @@
+/* Josh Hartshorn
+   Lab 13 - Linked Lists!!
+*/
+#include <iostream>
+
+using namespace std;
+class LLS{
+private:
+string str;
+LLS* next;
+public:
+  LLS(string s);
+  LLS(string s, LLS *rest);
+
+};
+LLS::LLS(string s){
+
+
+}
+
+int main(){
+
+  return 0;
+}
