08d093878827b8c2d3122ed6437954f5a987aba5
Joshua Hartshorn <woodne@Nicholass-MacBook-Pro-2.local>
2013-05-25 22:38:51 -0700

    lab 14 initial commit

---
+++ lab14/lab14.cpp
@@ -0,0 +1,19 @@
+/* Josh Hartshorn
+   CS120 Lab 14
+
+   Greatest Common Divisor Lab
+*/
+
+#include <iostream>
+using namespace std;
+
+int gcd(){
+  //this function will calculate the greatest common divisor of two positive integers using recursion
+
+  return 0;
+}
+
+int main(){
+  //main should take two command line arguments (the two integers)
+  return 0;
+}
