70b4e981e29a1cc4c7f40403b053f926b7df41fa
Joshua Hartshorn <woodne@Nicholass-MacBook-Pro-2.local>
2013-05-26 18:00:14 -0700

    need to add command line arguments

--- lab14/lab14.cpp
+++ lab14/lab14.cpp
@@ -7,13 +7,23 @@
 #include <iostream>
 using namespace std;
 
-int gcd(){
+int gcd(int a, int b){
   //this function will calculate the greatest common divisor of two positive integers using recursion
-
+  if (b == 0){
+    return a;
+  }
+  else{ 
+    return (b,a%b);
+  }
+  
   return 0;
 }
 
 int main(){
   //main should take two command line arguments (the two integers)
+  int a = 10;
+  int b = 20;
+  cout << gcd(a,b) << endl;
+
   return 0;
 }
